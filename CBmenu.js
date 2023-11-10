cb44fec6b4147a5e25be1e795da560acf45d1362
Walter Palacios <138240963+WalterCoder@users.noreply.github.com>
2023-11-10 17:40:37 -0600

    Add files via upload

---
+++ CBmenu.js
@@ -0,0 +1,31 @@
+//NAVBAR
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+  addEventListener("click",() =>{
+hamburger.classList.remove("active");
+navMenu.classList.remove("active");
+
+  }))
+
+  //GALLERY
+
+  var fullImgBox = document.getElementById("fullImgBox");
+  var fullImg = document.getElementById("fullImg");
+
+  function OpenFullImg(pic){
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+    
+  }
+
+  function closeFullImg(){
+    fullImgBox.style.display ="none";
+  }
